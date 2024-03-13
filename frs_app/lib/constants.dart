8820273cbb72b2c14a447c6de6fcad8f6563bb22
harshrajurkar <harshrajurkar201@gmail.com>
2024-03-13 16:11:40 +0530

    hidden creds

---
+++ frs_app/lib/constants.dart
@@ -0,0 +1,7 @@
+const apikey = "AIzaSyA3KWYai7N4oWEbAOa9GGsbGMEVJqgodfQ";
+const authdomain ="fertilizer-recommendatio-f9b82.firebaseapp.com";
+const databaseURL= "https://fertilizer-recommendatio-f9b82-default-rtdb.firebaseio.com";
+const projectId= "fertilizer-recommendatio-f9b82";
+const storageBucket= "fertilizer-recommendatio-f9b82.appspot.com";
+const messagingSenderId= "219091452172";
+const appId= "1:219091452172:web:3a36b7a06c77a195530929";
--- frs_app/lib/main.dart
+++ frs_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:frs_app/constants.dart';
 import 'package:frs_app/frs_app.dart';
 
 Future<void> main() async {
@@ -12,13 +13,12 @@ Future<void> main() async {
     if (kIsWeb) {
       await Firebase.initializeApp(
         options: FirebaseOptions(
-          apiKey: "AIzaSyA3KWYai7N4oWEbAOa9GGsbGMEVJqgodfQ",
-          authDomain: "fertilizer-recommendatio-f9b82.firebaseapp.com",
-          databaseURL: "https://fertilizer-recommendatio-f9b82-default-rtdb.firebaseio.com",
-          projectId: "fertilizer-recommendatio-f9b82",
-          storageBucket: "fertilizer-recommendatio-f9b82.appspot.com",
-          messagingSenderId: "219091452172",
-          appId: "1:219091452172:web:3a36b7a06c77a195530929",
+          apiKey: apikey,
+          authDomain: databaseURL,
+          projectId: projectId,
+          storageBucket: storageBucket,
+          messagingSenderId: messagingSenderId,
+          appId: appId,
         ),
       );
     } else {
